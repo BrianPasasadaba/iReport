4a0292b25b84ec0b48135884b46bccfa234c3791
BrianPasasadaba <briancyberrpasasadaba@iskolarngbayan.pup.edu.ph>
2024-05-10 14:34:24 +0800

    SIR-28 updated sql file

--- cdrrmo.sql
+++ cdrrmo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 09, 2024 at 02:48 PM
+-- Generation Time: May 10, 2024 at 08:32 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -42,7 +42,7 @@ CREATE TABLE `admins` (
 --
 
 INSERT INTO `admins` (`admin_id`, `last_name`, `first_name`, `middle_name`, `name_suffix`, `email`, `password`) VALUES
-(3, NULL, NULL, NULL, NULL, 'admin@cdrrmo.com', '$2b$12$f.g/WgSBWFaiCqTM8CnlJ.neGyJKDpDrTc2VJ7E1csyicXXffVm7.');
+(3, 'Admin', 'CDRRMO', NULL, NULL, 'admin@cdrrmo.com', '$2b$12$f.g/WgSBWFaiCqTM8CnlJ.neGyJKDpDrTc2VJ7E1csyicXXffVm7.');
 
 -- --------------------------------------------------------
 
@@ -66,6 +66,15 @@ CREATE TABLE `category` (
   `categories` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`category_id`, `categories`) VALUES
+(1, 'Medical Emergency'),
+(2, 'Vehicular Accident'),
+(3, 'Other Emergency');
+
 -- --------------------------------------------------------
 
 --
@@ -78,16 +87,27 @@ CREATE TABLE `reports` (
   `name` varchar(255) DEFAULT NULL,
   `phone_number` varchar(13) NOT NULL,
   `location` varchar(255) NOT NULL,
-  `latitude` decimal(10,8) DEFAULT NULL,
-  `longitude` decimal(10,8) DEFAULT NULL,
+  `latitude` decimal(11,8) DEFAULT NULL,
+  `longitude` decimal(11,8) DEFAULT NULL,
   `estimate_victims` int(11) NOT NULL,
   `report_details` text NOT NULL,
   `pictures` varchar(255) DEFAULT NULL,
-  `status_id` int(11) DEFAULT NULL,
+  `status_id` int(11) DEFAULT 1,
   `category_id` int(11) DEFAULT NULL,
   `responder_report` varchar(255) DEFAULT NULL,
   `cluster_distance` float DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `reports`
+--
+
+INSERT INTO `reports` (`report_id`, `date_time`, `name`, `phone_number`, `location`, `latitude`, `longitude`, `estimate_victims`, `report_details`, `pictures`, `status_id`, `category_id`, `responder_report`, `cluster_distance`) VALUES
+(3, '2024-05-10 00:26:01', 'Pasasadaba, Brian Cyber R.', '0949-593-1396', 'San Isidro Heights, Banlic, Cabuyao, Laguna', 14.23554500, 121.13740000, 3, 'Several family members fainted due to heatstroke, pls send help', NULL, 1, 1, NULL, NULL),
+(5, '2024-05-10 12:27:13', 'Pasasadaba Brian Cyber R', '0949-593-1396', 'San Isidro Heights, Banlic, Cabuyao, Laguna', 14.23554500, 121.13740000, 21, 'my mother slipped and fell on the floor, she is bleeding and unresponsive please send help', NULL, 1, 1, NULL, NULL),
+(6, '2024-05-10 12:32:44', 'Pasasadaba Brian Cyber R', '0949-593-1396', 'San Isidro Heights, Banlic, Cabuyao, Laguna', 14.23554500, 121.13740000, 3, 'there is a fire in the neighborhood, some people are still trapped send help immediately', NULL, 1, 3, NULL, NULL),
+(7, '2024-05-10 12:37:12', 'Juan Reyes', '0912-345-6789', 'San Isidro Heights, Banlic, Cabuyao, Laguna', 14.23564700, 121.13735200, 4, 'a car got into a collision with a motorbike here, send responders', NULL, 1, 1, NULL, NULL),
+(9, '2024-05-10 14:01:59', 'John R Doe', '0949-593-1396', 'San Isidro Heights, Banlic, Cabuyao, Laguna', 14.23554500, 121.13740000, 1, 'my brother is tripped and fell, his head is now bleeding profusely, we need an ambulance', NULL, 1, 1, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -99,6 +119,15 @@ CREATE TABLE `status` (
   `status_id` int(11) NOT NULL,
   `report_status` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `status`
+--
+
+INSERT INTO `status` (`status_id`, `report_status`) VALUES
+(1, 'Pending'),
+(2, 'Ongoing'),
+(3, 'Resolved');
 
 --
 -- Indexes for dumped tables
@@ -151,19 +180,19 @@ ALTER TABLE `admins`
 -- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
-  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `reports`
 --
 ALTER TABLE `reports`
-  MODIFY `report_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `report_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `status`
 --
 ALTER TABLE `status`
-  MODIFY `status_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `status_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Constraints for dumped tables
