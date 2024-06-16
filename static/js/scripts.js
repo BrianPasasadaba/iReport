fc557d19ac1d0e9bf1cb398e63115020995c39ef
anapup-web <anarosellerpulido@iskolarngbayan.pup.edu.ph>
2024-06-16 19:41:02 +0800

    fix: bug fix on the button on the landing page of the citizen SIR-52

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,19 +1,27 @@
 console.log("scripts.js loaded!");
 
 $(document).ready(function () {
-
-  
-
-  // Accordion Toggle Icon & Header Color Change (Updated for Bootstrap 5)
-  $('.accordion').on('hidden.bs.collapse', function () {
-    $(this).find('.fa-chevron-up').removeClass('fa-chevron-up').addClass('fa-chevron-down');
+  // Make the entire card-header clickable
+  $(".card-header").click(function () {
+    var target = $(this).find("button").attr("data-bs-target");
+    $(target).collapse("toggle");
   });
 
-  $('.accordion').on('shown.bs.collapse', function () {
-    $(this).find('.fa-chevron-down').removeClass('fa-chevron-down').addClass('fa-chevron-up');
+  // Accordion Toggle Icon & Header Color Change (Updated for Bootstrap 5)
+  $(".accordion").on("hidden.bs.collapse", function () {
+    $(this)
+      .find(".fa-chevron-up")
+      .removeClass("fa-chevron-up")
+      .addClass("fa-chevron-down");
+  });
+
+  $(".accordion").on("shown.bs.collapse", function () {
+    $(this)
+      .find(".fa-chevron-down")
+      .removeClass("fa-chevron-down")
+      .addClass("fa-chevron-up");
   });
 
   // No changes needed for dropdown toggle initialization (Bootstrap 5 compatible)
   $(".dropdown-toggle").dropdown();
-
 });
